abf00b2a9eaf6ba3bb2184be257a4b24fc15aa41
proyogram <proyogram@gmail.com>
2023-12-04 23:29:09 +0900

    Description を修正

--- modules/codepipeline/variables.tf
+++ modules/codepipeline/variables.tf
@@ -1,14 +1,14 @@
 variable "prefix" {
   type        = string
-  description = "全てのリソースの文字前に付与される値"
+  description = "全てのリソースの文字前に付与される値。"
 }
 
 variable "full_repository_id" {
   type        = string
-  description = "「Organization名/リポジトリ名」の形式"
+  description = "Codepipelineのソースとして設定するリポジトリ名。「ユーザ名/リポジトリ名」の形式で記載する。"
 }
 
 variable "branch_name" {
   type        = string
-  description = "ブランチ名"
+  description = "Codepipelineのソースとして設定するブランチ名。"
 }
